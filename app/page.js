55568de61d6adae3e53712436723ec4a6eb786c9
guno517 <guno5170@gmail.com>
2023-05-09 18:43:05 +0900

    초기 세팅

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="main">
+      <h4 className="title">지오 Shop</h4>
+      <p className="title-sub">made by gio</p>
+    </div>
+  );
+}
