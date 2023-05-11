7a101edfdf55220f1aeccd2a42ee7bbd0caabea7
guno517 <guno5170@gmail.com>
2023-05-11 19:34:39 +0900

    레이아웃 적용

--- app/page.js
+++ app/page.js
@@ -1,6 +1,12 @@
+import Link from 'next/link';
+
 export default function Home() {
   return (
     <div className="main">
+      {/* <div className="navbar">
+        <Link href="/">홈</Link>
+        <Link href="/productList">PorductList</Link>
+      </div> */}
       <h4 className="title">지오 Shop</h4>
       <p className="title-sub">made by gio</p>
     </div>
