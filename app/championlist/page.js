76af8a5ff99572ba94f4e68891bc7dda0a3132ba
guno517 <guno5170@gmail.com>
2023-05-12 23:00:02 +0900

    Image 태그 적용

--- app/championlist/page.js
+++ app/championlist/page.js
@@ -1,4 +1,7 @@
+import Image from 'next/image';
+import champImg from 'public/garen.jpg';
 import React from 'react';
+import '../globals.css';
 
 export default function ChampionList() {
   const champions = ['가렌', '갈리오', '갱플랭크'];
@@ -7,6 +10,11 @@ export default function ChampionList() {
       <h4 className="title">상품 목록</h4>
       {champions.map((champ, idx) => (
         <div key={idx} className="champ">
+          <Image
+            src={champImg}
+            alt="챔피언 이미지"
+            className="champ-img"
+          />
           <h4>{champ} $50</h4>
         </div>
       ))}
