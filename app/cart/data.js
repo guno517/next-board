09e4e31be1d42f32fd2e079a8999ba3b93684e07
guno517 <guno5170@gmail.com>
2023-05-13 19:21:25 +0900

    장바구니 컴포넌트화

---
+++ app/cart/data.js
@@ -0,0 +1,6 @@
+let price = 450;
+let count = 1;
+
+export { price, count };
+
+// export default price;
--- app/cart/page.js
+++ app/cart/page.js
@@ -1,6 +1,29 @@
 import React from 'react';
 import '../globals.css';
+import { price, count } from './data';
+import Hello from './hello';
 
 export default function Cart() {
-  return <h3 className="title">장바구니 페이지입니다</h3>;
+  return (
+    <>
+      <h3 className="title">장바구니 페이지입니다</h3>
+      <Hello />
+      <div className="cart-item">
+        <p>상품명</p>
+        <p>가격</p>
+        <p>수량</p>
+      </div>
+      <CartItem />
+    </>
+  );
+}
+
+function CartItem() {
+  return (
+    <div className="cart-item">
+      <p>가렌</p>
+      <p>{price}BP</p>
+      <p>{count}개</p>
+    </div>
+  );
 }
